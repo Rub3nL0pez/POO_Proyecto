5e7ae2fa6cc0fb44d0f9472fb94d285f3558d277
ruben <ruben@Pyjehyi>
2026-05-28 15:53:14 -0300

    usuario y memorycard se añadio

--- src/puzlecompuertaslogicas/Memorycard.java
+++ src/puzlecompuertaslogicas/Memorycard.java
@@ -3,11 +3,72 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package puzlecompuertaslogicas;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 /**
  *
  * @author ruben
  */
 public class Memorycard {
+    private static final String ARCHIVO_GUARDADO = "usuarios_db.dat";
+    //Carga la lista completa de usuarios registrados desde el disco
+    public static ArrayList<Usuario> cargarTodosLosUsuarios() {
+        File archivo = new File(ARCHIVO_GUARDADO);
+        if (!archivo.exists()) {
+            return new ArrayList<>(); // Retorna lista vacía si es la primera ejecución
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo))) {
+            return (ArrayList<Usuario>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error crítico al cargar la base de datos de usuarios: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+    //lista actualizada de usuarios
+    private static void guardarListaUsuarios(ArrayList<Usuario> lista) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ARCHIVO_GUARDADO))) {
+            oos.writeObject(lista);
+            System.out.println("Base de datos actualizada ");
+        } catch (IOException e) {
+            System.err.println("Error al guardar datos de usuarios: " + e.getMessage());
+        }
+    }
+    //Registra un usuario nuevo
+    public static void guardarOActualizarUsuario(Usuario usuarioAEditar) {
+        ArrayList<Usuario> lista = cargarTodosLosUsuarios();
+        int indiceEncontrado = -1;
+
+        // Buscamos mediante el nombre (ignorando mayúsculas) si ya está registrado
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getNombre().equalsIgnoreCase(usuarioAEditar.getNombre())) {
+                indiceEncontrado = i;
+                break;
+            }
+        }
+        if (indiceEncontrado != -1) {
+            // Si ya existía, reemplazamos sus datos viejos con el progreso nuevo
+            lista.set(indiceEncontrado, usuarioAEditar);
+        } else {
+            // Si no existía, significa que es un registro nuevo y lo sumamos a la lista
+            lista.add(usuarioAEditar);
+        }
+
+        // Guardamos la lista actualizada físicamente en el disco
+        guardarListaUsuarios(lista);}
+        //Obtiene la lista de todos los usuarios registrados, pero ordenada
+        public static ArrayList<Usuario> obtenerRankingUsuarios() {
+        ArrayList<Usuario> lista = cargarTodosLosUsuarios();
+        
+        // Ordenación eficiente de mayor a menor puntaje
+        lista.sort((u1, u2) -> Integer.compare(u2.getPuntaje(), u1.getPuntaje()));
+        
+        return lista;
+    }
     
 }
--- src/puzlecompuertaslogicas/Usuario.java
+++ src/puzlecompuertaslogicas/Usuario.java
@@ -3,11 +3,80 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package puzlecompuertaslogicas;
+import java.io.Serializable;
 
 /**
  *
  * @author ruben
  */
-public class Usuario {
-    
+public class Usuario implements Serializable  {
+    //atributos
+    private String nombre;
+    private String password;
+    private int nivelActual;
+    private int nivelMaximo;
+    private int puntaje;
+
+    public Usuario(String nombre, String password) {
+        //se inicializa un usuario 
+        this.nombre = nombre;
+        this.password = password;
+        this.nivelActual = 1;
+        this.nivelMaximo = 1;
+        this.puntaje = 0;
+    }
+    //en caso de avanzar de nivel
+    public void avanzarNivel(){
+        this.nivelActual++;
+        if(this.nivelActual > this.nivelMaximo){
+            this.nivelMaximo= this.nivelActual;
+        }
+    }
+    //en caso de volver un nivel para repasar
+    public void retrocederVoluntario(){
+        if(this.nivelActual > 1){
+            this.nivelActual--;
+        }
+    }
+    //en caso de volver por perder las vidas
+    public void retrocederFallo(){
+        if(this.nivelActual > 1){
+            this.nivelActual--;
+            this.nivelMaximo = this.nivelActual;
+        }
+    }
+    //verificacion de cuenta
+    public boolean verificarPassword(String passwordUsuario){
+        return this.password.equals(passwordUsuario);
+    }
+    //suma de puntos
+    public void sumarPuntos(int puntos){
+        if(puntos > 0){
+            this.puntaje += puntos;
+        }
+    }
+    //resta de puntos
+    public void restarPuntos(int puntos){
+        if(puntos > 0){
+            this.puntaje -= puntos;
+        }
+        if(this.puntaje < 0){
+            this.puntaje = 0;
+        }
+    }
+    //getter y setter
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public int getNivelActual() { return nivelActual; }
+    public void setNivelActual(int nivelActual) { this.nivelActual = nivelActual; }
+
+    public int getNivelMaximo() { return nivelMaximo; }
+   public void setNivelMaximo(int nivelMaximo) { this.nivelMaximo = nivelMaximo; }
+
+    public int getPuntaje() { return puntaje; }
+    public void setPuntaje(int puntaje) { this.puntaje = puntaje; }
 }
